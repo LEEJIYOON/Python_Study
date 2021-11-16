9066cbfd3feb1b3f275504abbd2ae50e1d9d13b4
easyyoon <wldbs0904@gmail.com>
2021-11-16 21:44:01 +0900

    Create quick_sort.py
    quick_sort

---
+++ Algorithm/quick_sort.py
@@ -0,0 +1,14 @@
+
+
+def quick_sort(array):
+  if len(array)<=1:
+    return array
+  
+  pivot=array[0]
+  tail=[1:]
+  
+  left_side=[x for x if x<=pivot]
+  right_side=[x for x if x>pivot]
+  
+  
+  return quick_sort(left_side) + [pivot] + quick_sort(right_side)
