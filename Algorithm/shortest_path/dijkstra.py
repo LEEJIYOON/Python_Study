e6c8c3ded1262935915ba8258b820152f8ba6952
easyyoon <wldbs0904@gmail.com>
2021-12-20 19:59:34 +0900

    Create dijkstra.py
    heapq basic code

---
+++ Algorithm/shortest_path/dijkstra.py
@@ -0,0 +1,30 @@
+import heapq #탐색을 최소화하기 위해, heapq을 사용
+
+
+n,m=map(int,input().split()) #n은 node 갯수, m은 간선 갯수
+start=int(input()) #시작점
+
+INF=int(1e9)
+graph=[[]*(n+1) for i range(n+1)] #node 넘버와 graph 행 맞추기위해 n+1
+distance=[INF]*(n+1)
+
+for _ in range(m):
+  a,b,c=map(int,input().split())
+  graph[a].append((b,c))
+  
+  
+def dijkstra(start):
+  q=[] #heapq은 앞에가 거리, 뒤에가 노드
+  distance[start]=0
+  heapq.heappush(h,(distance[start],start))
+  while q:
+    dist,now=heapq.heappop(q)
+    
+    if distance[now] < dist:
+      continue
+    
+    for i in graph[now]:
+      cost=dist+i[1]
+      if distance[i[0]] > cost: # graph는 앞에가 node 뒤에가 거리
+        distance[i[0]]=cost
+        heapq.heappush(q,cost,i[0])
